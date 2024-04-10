f771ef4b4d08fe95d96b979ca5498cef7d683581
ThisIsDemetrio <demetrio.marino1985@gmail.com>
2024-04-10 21:02:34 +0200

    feat: add role in user

--- models/User.py
+++ models/User.py
@@ -4,6 +4,8 @@ class User(BaseModel):
     username: str
     email: str | None = None
     disabled: bool | None = None
+    # TODO: This should be either "admin" or "user", can we enforce that?
+    role: str
 
 class UserInDB(User):
     hashed_password: str
