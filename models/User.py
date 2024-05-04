3cdebd0d3bf96096de88aa86075033f79b7972ef
ThisIsDemetrio <demetrio.marino1985@gmail.com>
2024-05-04 19:26:32 +0200

    feat: add validation on User model

--- models/User.py
+++ models/User.py
@@ -1,12 +1,21 @@
 from pydantic import BaseModel
 
 
+def validate_role(value):
+    if value not in ["admin", "user"]:
+        raise ValueError(f"Role must be either 'admin' or 'user', not {value}")
+    return value
+
+
 class User(BaseModel):
     username: str
     email: str | None = None
     disabled: bool | None = None
-    # TODO: This should be either "admin" or "user", can we enforce that?
     role: str
+
+    @classmethod
+    def __get_validators__(cls):
+        yield validate_role
 
 
 class UserInDB(User):
